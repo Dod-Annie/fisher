4c8a168dcd811f1c6ed1f027e50141c68ed97539
Dod-Annie <936166900@qq.com>
2019-02-13 00:07:08 +0800

    add route

--- fisher.py
+++ fisher.py
@@ -1,5 +1,19 @@
-from flask import  Flask
+from flask import  Flask, make_response
 
 app = Flask(__name__)
+app.config.from_object('config')
 
-app.run()
+@app.route('/hello')
+def hello():
+    headers={
+        'content-type':'text/plain',
+        'location':'http://www.bing.com'
+    }
+    response = make_response('<html></html>')
+    response.headers = headers
+    return response
+    # return 'Hello World'
+
+# app.add_url_rule('/hello',view_func=hello)
+
+app.run(host='0.0.0.0',debug=app.config['DEBUG'])
